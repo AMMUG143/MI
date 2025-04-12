bb71d669a7e507b5a2eeba8e148d6ac4040bd1cd
AMMUG143 <samrutha958@gmail.com>
2025-04-12 06:26:28 -0800

    adding a new file

---
+++ eve.py
@@ -0,0 +1,9 @@
+def eve_numbers():
+    """Eve numbers are even numbers."""
+    num1 = 2
+    num2 = 4
+    result = num1 + num2
+    return result
+
+if __name__ == "__main__":
+    print("The sum is:", eve_numbers())
